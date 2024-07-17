a74e838531ade8c18c6744fe29cfb739700a4e30
Emile Tremblay <emiletremblay01@gmail.com>
2024-07-17 02:45:51 -0400

    fix: add trycatch to api/unlock logic

--- app/api/unlock/route.ts
+++ app/api/unlock/route.ts
@@ -1,20 +1,26 @@
 import { type NextRequest, NextResponse } from "next/server";
 import { isNipOk } from "@/data/user";
 export async function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
-  const origin = request.nextUrl.origin;
+  try {
+    const searchParams = request.nextUrl.searchParams;
+    const origin = request.nextUrl.origin;
+    console.log("origin", origin);
+    const nip = searchParams.get("nip");
 
-  const nip = searchParams.get("nip");
+    if (!nip) {
+      return new NextResponse("Missing query parameter NIP", { status: 400 });
+    }
 
-  if (!nip) {
-    return new NextResponse("Missing query parameter NIP", { status: 400 });
+    console.log("nip", nip);
+    const nipOk = isNipOk(nip, origin);
+
+    if (!nipOk) {
+      return new NextResponse("Invalid NIP", { status: 403 });
+    }
+
+    return new NextResponse("Nip ok!", { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new NextResponse("An error occurred!", { status: 500 });
   }
-
-  const nipOk = isNipOk(nip, origin);
-
-  if (!nipOk) {
-    return new NextResponse("Invalid NIP", { status: 403 });
-  }
-
-  return new NextResponse("Nip ok!", { status: 200 });
 }
