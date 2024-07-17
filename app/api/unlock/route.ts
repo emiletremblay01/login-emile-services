233051587847487d55c1da03d42d73260a61501a
Emile Tremblay <emiletremblay01@gmail.com>
2024-07-17 02:32:36 -0400

    fix: update api/unlock logic

--- app/api/unlock/route.ts
+++ app/api/unlock/route.ts
@@ -1,12 +1,20 @@
-import { type NextRequest } from "next/server";
-
-export function GET(request: NextRequest) {
+import { type NextRequest, NextResponse } from "next/server";
+import { isNipOk } from "@/data/user";
+export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
-  const nextUrl = request.nextUrl;
-  console.log(nextUrl);
-  const query = searchParams.get("nip");
-  if (!query) {
-    return new Response("Missing query parameter NIP", { status: 400 });
+  const origin = request.nextUrl.origin;
+
+  const nip = searchParams.get("nip");
+
+  if (!nip) {
+    return new NextResponse("Missing query parameter NIP", { status: 400 });
   }
-  return new Response(`Query parameter NIP is: ${query}`);
+
+  const nipOk = isNipOk(nip, origin);
+
+  if (!nipOk) {
+    return new NextResponse("Invalid NIP", { status: 403 });
+  }
+
+  return new NextResponse("Nip ok!", { status: 200 });
 }
--- data/user.ts
+++ data/user.ts
@@ -27,3 +27,31 @@ export const getUserById = async (id: string) => {
     return null;
   }
 };
+
+export const isNipOk = async (nip: string, origin: string) => {
+  try {
+    const user = await prismadb.user.findFirst({
+      where: {
+        websites: {
+          some: {
+            url: {
+              equals: origin,
+              mode: "insensitive", // Enable case-insensitive search
+            },
+          },
+        },
+      },
+    });
+
+    const website = user?.websites.find((website) => {
+      if (website.url === origin) {
+        return website;
+      }
+    });
+
+    return website?.nip === nip;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
